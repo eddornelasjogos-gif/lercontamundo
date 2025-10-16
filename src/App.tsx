21bb980885cbc1a924bdd560a2c9e41a6bd5de3e
[dyad] <eddornelasjogos@gmail.com>
2025-10-16 14:55:55 -0300

    [dyad] Corrigido erro de contexto do React Router - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,11 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ProgressProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
+    <BrowserRouter>
+      <ProgressProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
           <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
@@ -31,9 +31,9 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </ProgressProvider>
+        </TooltipProvider>
+      </ProgressProvider>
+    </BrowserRouter>
   </QueryClientProvider>
 );
 
