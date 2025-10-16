1b9d59460389a6cdc51fc879a8bebbcaca064503
[dyad] <eddornelasjogos@gmail.com>
2025-10-16 15:14:55 -0300

    [dyad] Corrigido erro de importação de rota - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProgressProvider } from "@/contexts/ProgressContext";
 import Index from "./pages/Index";
-import Reading from "./pages/Reading";
 import Math from "./pages/Math";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
@@ -26,7 +25,6 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/journey" element={<Journey />} />
-            <Route path="/reading" element={<Reading />} />
             <Route path="/reading/:id" element={<Story />} />
             <Route path="/math/:challengeId" element={<Math />} />
             <Route path="/profile" element={<Profile />} />
