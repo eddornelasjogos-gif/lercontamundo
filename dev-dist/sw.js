8b307b01778b7a58dab13d1298e015857b130827
[dyad] <eddornelasjogos@gmail.com>
2025-10-18 17:58:46 -0300

    [dyad] Adicionando 12 capturas de tela ao manifesto do PWA. - wrote 13 file(s) + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c6a197bf'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.aj24l1d2qug"
+    "revision": "0.g77o9i1pra"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
