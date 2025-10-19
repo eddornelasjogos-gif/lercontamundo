367e579efb3872e77ebe07b306219bb15b582a06
[dyad] <eddornelasjogos@gmail.com>
2025-10-19 09:56:41 -0300

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c6a197bf'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3l71e7gpc28"
+    "revision": "0.9m542g0haj8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
